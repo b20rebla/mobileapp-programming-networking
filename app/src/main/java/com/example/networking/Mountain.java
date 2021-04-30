8cc2b73e99db67d3eb965cd68dc9fb85144ee425
Rebecka <b20rebla@student.his.se>
2021-04-30 10:31:51 +0200

    Två java klasser skapades

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,17 @@
+package com.example.networking;
+
+public class Mountain {
+
+    //Genom denna koden har vi gett förutsättningarna för att kunna låta gson biblioteket skapa våra berg
+    private String ID;
+    private String name;
+    private String type;
+    private String company;
+    private String location;
+    private String category;
+
+    private int size;
+    private int cost;
+
+    private Auxdata auxdata;
+}
