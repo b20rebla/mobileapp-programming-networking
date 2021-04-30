40565fee508daafffc39c968a2d5781342128fbe
Rebecka <b20rebla@student.his.se>
2021-04-30 12:02:03 +0200

    Returnerar värden som kommer finnas i MainActivity

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -14,10 +14,27 @@ public class Mountain {
     private int cost;
 
     private Auxdata auxdata;
+
     //Vill kalla atrributen för detta, namnger, måste ha detta för att kunna returnera våra värden sen som kommer med koden som visas nedanför
     public Mountain(String name,String type,String location){
         this.name = name;
         this.type = type;
         this.location= location;
+    }
+
+    public String getName( String name) {
+        return name;
+    }
+
+    public String getType( String type) {
+        return type;
+    }
+
+    public String getLocation(String location) {
+        return location;
+    }
+
+    public String toString(){
+        return name;
     }
 }
